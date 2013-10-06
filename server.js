8cbc686775d85f2a8470cb541be58eafa32bdc54
hiroo takaku <slgerkamp@gmail.com>
2013-10-06 19:43:09 +0900

    vol.2 リクエストを”ルーティング”　ブラウザがリクエストしてきたURLパスを出力

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // vol.1 基本的なHTTPサーバ
+// vol.2 リクエストを”ルーティング”
 
 // httpモジュールを読み込む（Node.jsに同梱されている）
 var http = require("http");
+var url = require("url");
 
 function start(){
 	// レスポンス処理を切り出す
 	function onRequest(req, res){
-		console.log("Request received.");
+		var pathname = url.parse(req.url).pathname;
+		console.log("Request for '" + pathname + "' received.");
 		res.writeHead(200,{"Content-Type":"text/plain"});
 		res.write("Hello, World");
 		res.end();
