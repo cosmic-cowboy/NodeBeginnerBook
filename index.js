caad8ec86c50d8dd212df51b9da818c4a402d9ad
hiroo takaku <slgerkamp@gmail.com>
2013-10-06 20:21:01 +0900

    vol.3 リクエストハンドラ リクエストハンドラによる応答

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // サーバモジュールを呼び出す
 var server = require("./server");
 var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+// リクエストハンドラの集合
+var handle = [];
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
 
 // exportされた関数を使う
-server.start(router.route);
+server.start(router.route, handle);
--- router.js
+++ router.js
@@ -1,8 +1,16 @@
 // リクエストルータ
 // ブラウザがリクエストしてきたURLパスを出力
 
-function route(pathname){
-  console.log("About to route a request for "+ pathname);
+function route(handle, pathname){
+	console.log("About to route a request for "+ pathname);
+
+	// パス名に対応するリクエストハンドラが存在するかどうかをチェック
+	// 存在する場合は、紐付けられた関数を呼び出す
+	if(typeof handle[pathname] === 'function'){
+		handle[pathname]();
+	} else {
+		console.log("No request handler found for " + pathname);
+	}
 }
 
 exports.route = route;
