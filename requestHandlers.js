441b6ff0a3eff79a8d22ba509f100f4c607a1e4b
hiroo takaku <slgerkamp@gmail.com>
2013-10-06 23:16:46 +0900

    vol.5 ファイルアップロードの扱い ファイルアップロードのHTML要素をフォームに追加

--- requestHandlers.js
+++ requestHandlers.js
@@ -12,9 +12,9 @@ function start(res, postData){
 		'charset=UTF-8" />'+
 		'</head>'+
 		'<body>'+
-		'<form action="/upload" method="post">'+
-		'<textarea name="text" rows="20" cols="60"></textarea>'+
-		'<input type="submit" value="Submit text" />'+
+		'<form action="/upload" enctype="multipart/form-data" method="post">'+
+		'<input type="file" name="upload">'+
+		'<input type="submit" value="Upload file" />'+
 		'</form>'+
 		'</body>'+
 		'</html>';
