7fc97667cb777dfeb385ac4110912a1e2baf1e85
hiroo takaku <slgerkamp@gmail.com>
2013-10-06 22:38:21 +0900

    vol.4 POSTリクエストの扱い textareaフォーム

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,25 @@
 // リクエストハンドラ
-var exec = require("child_process").exec;
 
 function start(res){
 	console.log("Request handler 'start' was called.");
 
-	var content = "empty";
+	var body ='<html>'+
+		'<head>'+
+		'<meta http-equiv="Content-Type" content="text/html; '+
+		'charset=UTF-8" />'+
+		'</head>'+
+		'<body>'+
+		'<form action="/upload" method="post">'+
+		'<textarea name="text" rows="20" cols="60"></textarea>'+
+		'<input type="submit" value="Submit text" />'+
+		'</form>'+
+		'</body>'+
+		'</html>';
 
-	exec("ls -lah",
-		{ timeout:100000, maxBuffer:20000*1024},
-		function (error, stdout, stderr) {
-			res.writeHead(200,{"Content-Type":"text/plain"});
-			res.write(stdout);
-			res.end();
-		}
-	);
+
+	res.writeHead(200,{"Content-Type":"text/html"});
+	res.write(body);
+	res.end();
 }
 
 function upload(res){
